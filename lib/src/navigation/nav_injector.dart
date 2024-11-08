304b8419176602b545e41b0f56f7fb8eaff4772c
Vinicius Cruvinel <vinicius.cruvinel@ilia.digital>
2024-11-08 12:17:12 -0300

    refactor: change NavInjector

--- lib/src/navigation/nav_injector.dart
+++ lib/src/navigation/nav_injector.dart
@@ -16,12 +16,26 @@ class NavInjector {
 
   /// Resolve uma dependência registrada
   T resolve<T>(String key) {
-    return _dependencies[key] as T;
+    final dependency = _dependencies[key];
+    if (dependency is T) {
+      return dependency;
+    }
+    throw ArgumentError(
+        'Dependência de tipo ${T.toString()} não encontrada para a chave $key');
   }
 
   /// Resolve uma rota registrada
   Widget Function()? resolveRoute(String route) {
-    return _routes[
-        route]; // Retorna a função de construção da página (widget builder)
+    final pageBuilder = _routes[route];
+    if (pageBuilder != null) {
+      return pageBuilder;
+    }
+    print('Rota não encontrada: $route');
+    return _routes['/']; // Página padrão ou de erro
+  }
+
+  /// Método para registrar uma rota padrão (caso o usuário não tenha definido)
+  void registerDefaultRoute(Widget Function() pageBuilder) {
+    _routes['/'] = pageBuilder;
   }
 }
