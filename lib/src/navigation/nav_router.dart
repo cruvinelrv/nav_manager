83570830b3e200e32f60410524a0d40b949b4948
Vinicius Cruvinel <vinicius.cruvinel@ilia.digital>
2024-11-11 15:12:13 -0300

    refactor: change initializeRoutes

--- lib/src/navigation/nav_router.dart
+++ lib/src/navigation/nav_router.dart
@@ -5,14 +5,13 @@ class NavRouter extends RouterDelegate<RouteInformation>
     with ChangeNotifier, PopNavigatorRouterDelegateMixin<RouteInformation> {
   final NavInjector _injector;
   final Widget Function()? escapePageBuilder;
-  final List<Page> _pages =
-      []; // Aqui vamos garantir que as páginas sejam dinâmicas.
+  List<Page> _pages = [];
 
   @override
   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
   NavRouter(this._injector, {this.escapePageBuilder}) {
-    // As rotas serão registradas fora do construtor do NavRouter.
+    // Inicializar as rotas corretamente
     _initializeRoutes();
   }
 
@@ -21,51 +20,34 @@ class NavRouter extends RouterDelegate<RouteInformation>
 
   @override
   Widget build(BuildContext context) {
-    debugPrint('NavRouter build chamado');
     return Navigator(
       key: navigatorKey,
-      pages: List.of(_pages),
+      pages: _pages,
       onDidRemovePage: (result) {
-        debugPrint('onDidRemovePage chamado');
         popRoute();
       },
     );
   }
 
-  // Inicializar as rotas
+  // Inicializa as rotas com as páginas
   void _initializeRoutes() {
-    // Adicionar a página inicial na pilha
-    _pages.add(_buildInitialPage('/')); // Rota padrão "/"
-
-    // Adicionar a EscapePage como última página, caso necessário.
-    _pages.add(_buildEscapePage());
-  }
-
-  // Método para navegar para a rota
-  Future<void> to(String route) async {
-    debugPrint('Tentando navegar para a rota: $route');
-    final pageBuilder = _injector.resolveRoute(route);
-
-    if (pageBuilder != null) {
-      // Verifica se a rota já existe na pilha
-      if (!_pages.any((page) => page.key == ValueKey(route))) {
-        _addPage(route, pageBuilder);
-        debugPrint('Rota $route adicionada à pilha');
-      } else {
-        debugPrint('Rota $route já está na pilha');
+    // Registra as rotas e garante que a página inicial seja construída
+    final routes = _injector.getRoutes(); // Obter todas as rotas registradas
+    for (var route in routes) {
+      final pageBuilder =
+          _injector.resolveRoute(route); // Resolva a função para a rota
+      if (pageBuilder != null) {
+        _addPage(route, pageBuilder); // Adiciona a página à pilha
       }
-    } else {
-      debugPrint('Rota não encontrada: $route');
-      _pages.add(_buildEscapePage());
     }
 
-    // Notifica ouvintes após a alteração da pilha
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      notifyListeners();
-    });
+    // Se nenhuma rota foi adicionada (isso significa que não há rotas registradas), adiciona a EscapePage
+    if (_pages.isEmpty) {
+      _addEscapePage();
+    }
   }
 
-  // Adiciona uma nova página à pilha
+  // Adiciona uma página na pilha de navegação
   void _addPage(String route, Widget Function(NavInjector) pageBuilder) {
     _pages.add(MaterialPage(
       key: ValueKey(route),
@@ -73,20 +55,9 @@ class NavRouter extends RouterDelegate<RouteInformation>
     ));
   }
 
-  // Página inicial
-  MaterialPage _buildInitialPage(String route) {
-    debugPrint('Construindo a página inicial para a rota: $route');
-    return MaterialPage(
-      key: ValueKey('$route-${DateTime.now().millisecondsSinceEpoch}'),
-      child: _injector.resolveRoute(route)?.call(_injector) ??
-          const SizedBox.shrink(),
-    );
-  }
-
-  // Página de erro/escape para rotas não encontradas
-  MaterialPage _buildEscapePage() {
-    debugPrint('Construindo a EscapePage');
-    return MaterialPage(
+  // Página de Escape para quando a rota não for encontrada
+  void _addEscapePage() {
+    _pages.add(MaterialPage(
       key: const ValueKey('escape'),
       child: escapePageBuilder != null
           ? escapePageBuilder!()
@@ -95,36 +66,49 @@ class NavRouter extends RouterDelegate<RouteInformation>
               body: const Center(
                   child: Text('A rota solicitada não foi encontrada.')),
             ),
-    );
+    ));
+  }
+
+  // Método para navegar para a rota
+  Future<void> to(String route) async {
+    final pageBuilder = _injector.resolveRoute(route);
+
+    if (pageBuilder != null) {
+      // Verifica se a rota já existe na pilha
+      if (!_pages.any((page) => page.key == ValueKey(route))) {
+        _addPage(route, pageBuilder);
+      }
+    } else {
+      print('Rota não encontrada: $route');
+      _addEscapePage();
+    }
+
+    // Notifica ouvintes após a alteração da pilha
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      notifyListeners();
+    });
   }
 
   @override
-  Future<void> setNewRoutePath(RouteInformation configuration) {
-    throw UnimplementedError();
+  Future<void> setNewRoutePath(RouteInformation configuration) async {
+    final route = configuration.uri.path.isEmpty ? '/' : configuration.uri.path;
+    final pageBuilder = _injector.resolveRoute(route);
+
+    if (pageBuilder != null) {
+      _pages = [
+        MaterialPage(
+          key: ValueKey(route),
+          child: pageBuilder(_injector),
+        ),
+      ];
+    } else {
+      _addEscapePage();
+      _pages = List.of(_pages);
+    }
+
+    // Notifica ouvintes após a alteração da pilha
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      notifyListeners();
+    });
   }
-
-  // @override
-  // Future<void> setNewRoutePath(RouteInformation configuration) async {
-  //   final route = configuration.uri.path.isEmpty ? '/' : configuration.uri.path;
-  //   debugPrint('setNewRoutePath chamado para a rota: $route');
-  //   final pageBuilder = _injector.resolveRoute(route);
-
-  //   if (pageBuilder != null) {
-  //     _pages = [
-  //       MaterialPage(
-  //         key: ValueKey(route),
-  //         child: pageBuilder(_injector),
-  //       ),
-  //     ];
-  //     debugPrint('Rota $route carregada com sucesso');
-  //   } else {
-  //     _pages = [_buildEscapePage()];
-  //     debugPrint('Rota $route não encontrada. Redirecionando para a EscapePage');
-  //   }
-
-  //   // Notifica ouvintes após a alteração da pilha
-  //   WidgetsBinding.instance.addPostFrameCallback((_) {
-  //     notifyListeners();
-  //   });
-  // }
 }
