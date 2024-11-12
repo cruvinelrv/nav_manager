fc579a8d734f9b8e1d19300acb562959158168aa
Vinicius Cruvinel <vinicius.cruvinel@ilia.digital>
2024-11-12 08:20:22 -0300

    refactor: update nav_routes_config

--- lib/src/config/nav_routes_config.dart
+++ lib/src/config/nav_routes_config.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:nav_manager/src/navigation/nav_injector.dart';
 
 abstract class NavRoutesConfig {
-  Map<String, Widget Function(NavInjector)> get routes;
+  Map<String, Widget Function()> get routes;
 
   void defineRoutes();
 
-  void registerRoute(String route, Widget Function(NavInjector) pageBuilder) {
+  void registerRoute(String route, Widget Function() pageBuilder) {
     if (!routes.containsKey(route)) {
       routes[route] = pageBuilder;
     }
   }
 
-  Map<String, Widget Function(NavInjector)> getAllRoutes() {
+  Map<String, Widget Function()> getAllRoutes() {
     return routes;
   }
 }
